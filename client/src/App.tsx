c717458703b0f608981a163a2242d48dd6c11598
rockribeirao <rtodeschini@rockribeirao.com.br>
2026-09-07 10:05:21 -0300

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,46 @@
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/NotFound";
+import { Route, Switch } from "wouter";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Home from "./pages/Home";
+import TodosNoRock from "./pages/TodosNoRock";
+
+const FESTIVAL_HOSTS = ["todosnorock.com.br", "www.todosnorock.com.br"];
+
+function Router() {
+  // When accessed via todosnorock.com.br, serve the festival page for every
+  // path on that domain while keeping todosnorock.com.br in the address bar.
+  if (typeof window !== "undefined" && FESTIVAL_HOSTS.includes(window.location.hostname)) {
+    return (
+      <Switch>
+        <Route component={TodosNoRock} />
+      </Switch>
+    );
+  }
+
+  return (
+    <Switch>
+      <Route path={"/"} component={Home} />
+      <Route path={"/todos-no-rock"} component={TodosNoRock} />
+      <Route path={"/404"} component={NotFound} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="dark">
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
